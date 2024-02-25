adb0d69b860f571010062e83a8680170a984c0d1
pavan-pani <pavanpk1012@gmail.com>
2024-02-25 21:26:36 +0530

    parctice

--- react-hook-form/src/components/youtubeForm.tsx
+++ react-hook-form/src/components/youtubeForm.tsx
@@ -10,8 +10,15 @@ type formValues = {
   channel: string
 }
 const YoutubeForm = () => {
-  const form = useForm<formValues>()
-  const { register, control, handleSubmit } = form
+  const form = useForm<formValues>({
+    defaultValues: {
+      username: "",
+      email: "",
+      channel: ""
+    }
+  })
+  const { register, control, handleSubmit, formState } = form
+  const { errors } = formState
 
   const onSubmited = (data: formValues) => {
     console.log("form submited", data);
@@ -22,16 +29,39 @@ const YoutubeForm = () => {
     <div>
       <h1>Youtube from ({rendercount / 2})</h1>
       <form onSubmit={handleSubmit(onSubmited)} noValidate>
-        <label htmlFor='username'>Username</label>
-        {/* <input type='text' id='username' name={name} ref={ref} onChange={onChange} onBlur={onBlur} /> */}
-        <input type='text' id='username' {...register("username", {required:"please provide the User Name"})} />
+        <div className='form-control'>
+          <label htmlFor='username'>Username</label>
+          {/* <input type='text' id='username' name={name} ref={ref} onChange={onChange} onBlur={onBlur} /> */}
+          <input type='text' id='username' {...register("username", { required: "please provide the User Name" })} />
+          <p className='error'>{errors.username?.message}</p>
+        </div>
 
 
-        <label htmlFor='email'>E-mail</label>
-        <input type='email' id='email' {...register("email")} />
+        <div className='form-control'>
+          <label htmlFor='email'>E-mail</label>
+          <input
+            type='email'
+            id='email'
+            {...register("email", {
+              required: 'Email is required',
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: 'Invalid email address',
+              },
+              validate: (formvalue) => {
+                return formvalue !== "admin@email.com" || "enter a diffrent email address"
+              }
+            })
+            } />
+          <p className='error'>{errors.email?.message}</p>
+        </div>
 
-        <label htmlFor='channel'>Channel</label>
-        <input type='text' id='channel' {...register("channel", {required:"please provide the channel nmae"})} />
+        <div className='form-control'>
+          <label htmlFor='channel'>Channel</label>
+          <input type='text' id='channel' {...register("channel", { required: "please provide the channel nmae" })} />
+          <p className='error'>{errors.channel?.message}</p>
+        </div>
+
 
         <button>Submit</button>
       </form>
